9d312e4fb17e82e6731e35b07d089f28fd3f02c5
Undin <arseniy1993@mail.ru>
2014-12-10 00:24:23 +0300

    modified Method interface

--- src/main/java/com/ifmo/mathproject/Method.java
+++ src/main/java/com/ifmo/mathproject/Method.java
@@ -4,5 +4,5 @@ package com.ifmo.mathproject;
  * Created by warrior on 10.12.14.
  */
 public interface Method<D, T extends Layer<D>> {
-    public T nextLayer(T currentLayer);
+    public T nextLayer(T... previousLayers);
 }
