a34db2baa048b107937cda8abe42cf98d13369ea
Ivan Samborskiy <whiplash1993@yandex.ru>
2014-12-10 23:20:08 +0300

    init ui package (for javafx)

---
+++ src/main/java/com/ifmo/mathproject/ui/Window.java
@@ -0,0 +1,29 @@
+package com.ifmo.mathproject.ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by Whiplash on 10.12.2014.
+ */
+public class Window extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main_window.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("PDE");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
