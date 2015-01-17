c77b9db8970582f2794af108c4904de7ccc5d96e
Undin <arseniy1993@mail.ru>
2015-01-17 16:16:28 +0300

    correct handling of edge points

--- src/main/java/com/ifmo/mathproject/d1/PartialImplicitMethod.java
+++ src/main/java/com/ifmo/mathproject/d1/PartialImplicitMethod.java
@@ -29,11 +29,12 @@ public class PartialImplicitMethod extends Method1D {
     @Override
     public Layer1D nextLayer(Layer1D... previousLayers) {
         Layer1D prevLayer = previousLayers[0];
+        int size = prevLayer.size();
 
-        final double[] prev = new double[prevLayer.size()];
-        final double[] cur = new double[prevLayer.size()];
-        final double[] next = new double[prevLayer.size()];
-        final double[] freeCoef = new double[prevLayer.size()];
+        final double[] prev = new double[size];
+        final double[] cur = new double[size];
+        final double[] next = new double[size];
+        final double[] freeCoef = new double[size];
 
         final double[] prevLayerConcentration = prevLayer.getConcentration();
         final double[] prevLayerTemperature = prevLayer.getTemperature();
@@ -41,7 +42,7 @@ public class PartialImplicitMethod extends Method1D {
         double[] prevIterationTemperature = prevLayerTemperature;
 
         for (int it = 0; it < iteration; it++) {
-            for (int i = 0; i < prevLayer.size(); i++) {
+            for (int i = 0; i < size; i++) {
                 prev[i] = -model.getD();
                 cur[i] = dx2DivDt +
                         2 * model.getD() +
@@ -49,14 +50,20 @@ public class PartialImplicitMethod extends Method1D {
                 next[i] = -model.getD();
                 freeCoef[i] = dx2DivDt * prevLayerConcentration[i];
             }
+            freeCoef[0] -= prev[0] * prevIterationConcentration[0];
+            freeCoef[size - 1] -= next[size - 1] * prevIterationConcentration[size - 1];
+
             prevIterationConcentration = Utils.tridiagonalMatrixAlgorithm(prev, cur, next, freeCoef);
 
-            for (int i = 0; i < prevLayer.size(); i++) {
+            for (int i = 0; i < size; i++) {
                 prev[i] = -lambdaDivCDivP;
                 cur[i] = dx2DivDt + 2 * lambdaDivCDivP;
                 next[i] = -lambdaDivCDivP;
                 freeCoef[i] = dx2OnKOnQDivC * Math.pow(prevIterationConcentration[i], model.getAlpha()) * Math.exp(-EOnR / prevIterationTemperature[i]) + dx2DivDt * prevLayerTemperature[i];
             }
+            freeCoef[0] -= prev[0] * prevIterationTemperature[0];
+            freeCoef[size - 1] -= next[size - 1] * prevIterationTemperature[size - 1];
+
             prevIterationTemperature = Utils.tridiagonalMatrixAlgorithm(prev, cur, next, freeCoef);
         }
 
