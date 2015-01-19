a09f9eae3e5e1565612e491daa6eb81e6fcc1270
Undin <arseniy1993@mail.ru>
2015-01-20 00:24:22 +0300

    make Methos2D abstract

--- src/main/java/com/ifmo/mathproject/d2/Method2D.java
+++ src/main/java/com/ifmo/mathproject/d2/Method2D.java
@@ -6,14 +6,9 @@ import com.ifmo.mathproject.Model;
 /**
  * Created by warrior on 20.01.15.
  */
-public class Method2D extends Method<Model2D, double[][], Layer2D> {
+public abstract class Method2D extends Method<Model2D, double[][], Layer2D> {
 
     public Method2D(Model2D model) {
         super(model);
-    }
-
-    @Override
-    public Layer2D nextLayer(Layer2D... previousLayers) {
-        return null;
     }
 }
