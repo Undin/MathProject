5eb3aecbc33fe470a8bc89bc5071900533a526db
Undin <arseniy1993@mail.ru>
2015-01-20 00:22:00 +0300

    added size getters for Layer2D

--- src/main/java/com/ifmo/mathproject/d2/Layer2D.java
+++ src/main/java/com/ifmo/mathproject/d2/Layer2D.java
@@ -7,17 +7,23 @@ import com.ifmo.mathproject.Layer;
  */
 public class Layer2D extends Layer<double[][]> {
 
-    private final int size;
+    private final int sizeX;
+    private final int sizeY;
 
     public Layer2D(double[][] concentration, double[][] temperature) {
         super(concentration, temperature);
         if (concentration.length != temperature.length) {
             throw new IllegalArgumentException("concentration.length != temperature.length");
         }
-        size = concentration.length;
+        sizeX = concentration.length;
+        sizeY = concentration[0].length;
     }
 
-    public int size() {
-        return size;
+    public int getSizeX() {
+        return sizeX;
+    }
+
+    public int getSizeY() {
+        return sizeY;
     }
 }
